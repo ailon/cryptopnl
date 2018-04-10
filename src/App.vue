bc2eced94f1912efbbc2a020dfe3b0e398302312
Alan Mendelevich <ailon@ailon.org>
2018-04-10 17:34:44 +0300

    reworked and reformated initial and log adding section

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,10 @@
 
 <style lang="scss">
 @import '../node_modules/bootstrap/scss/bootstrap.scss';
+body {
+  background-color: #f8f8f8;
+}
+#app {
+  padding: 10px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,117 +1,159 @@
 <template>
   <div class="home container">
+    <header>
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1 class="display-2">Crypto P&L</h1>
+          <p class="lead">Calculate your crypto trading profit/loss in accounting(-ish) way.<sup>*</sup></p>
+        </div>
+      </div>
+    </header>
+
     <form>
-      <div class="row">
-        <div class="col-12">
-          <h1>Crypto P&L</h1>
+
+      <div class="card" v-show="step == 'intro'">
+        <h2 class="card-header">Important</h2>
+        <div class="card-body">
+          <h5 class="card-title">Please read carefully before proceeding...</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <ul>
+            <li>Limitation...</li>
+            <li>Limitation...</li>
+          </ul>
+          <button class="btn btn-success" @click.prevent="goToLogStep">I understand and this works for me, let's proceed!</button>
+          <p class="mt-2"><a href="#">This doesn't work for me and I can write JavaScript/TypeScript</a></p>
+          <p class="small">Doesn't work for you and not a developer? <a href="mailto:#">Send me an email</a> if you'd like to sponsor a generally useful feature.</p>
         </div>
+      </div>        
 
-        <div class="col-12">
-          <div class="form-group">
-            <label for="logFileInput">Select exchange log file (.csv)</label>
-            <input type="file" class="form-control-file" id="logFileInput" @change="setLogFile($event.target.files[0])">
-          </div>          
+      <div class="card border-danger mb-2" v-show="errors.length > 0">
+        <div class="card-header">
+          Errors processing: <a href="#" @click.prevent="showErrorDetails = !showErrorDetails">{{ errors.length }}</a>
         </div>
-        <div class="col-12">
-          <button type="button" class="btn btn-primary mr-1" @click="parseKrakenLog">Parse as Kraken log</button>
-          <button type="button" class="btn btn-primary" @click="parseBitstampLog">Parse as Bitstamp log</button>
-        </div>
-        
-      </div>
-
-      <div class="row">
-        <div class="col-12" style="max-height: 400px; overflow: scroll;">
-
-        <table class="table table-sm">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Date</th>
-              <th scope="col">Operation</th>
-              <th scope="col">Crypto</th>
-              <th scope="col">Volume</th>
-              <th scope="col">Price</th>
-              <th scope="col">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(trade, index) in tradeLog"
-              :key="index"
-            >
-              <th scope="row">{{ index + 1 }}</th>
-              <td>{{ formatDate(trade.timestamp) }}</td>
-              <td>{{ trade.operation }}</td>
-              <td>{{ trade.crypto }}</td>
-              <td>{{ trade.volume }}</td>
-              <td>{{ trade.price }}</td>
-              <td>{{ trade.volume * trade.price }}</td>
-            </tr>
-          </tbody>
-        </table>
-
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-12">
-          <button type="button" class="btn btn-danger" @click="calculatePnL">Calculate Profit/Loss</button>
-        </div>
-      </div>
-
-      <div class="row" v-show="errors.length > 0">
-        <div class="col-12">
+        <div class="card-body" v-show="showErrorDetails">
           <ul class="text-danger">
             <li v-for="(error, index) in errors" :key="index">{{error}}</li>
           </ul>
         </div>
       </div>
+      
 
-      <div class="row">
-        <div class="col-12" style="max-height: 400px; overflow: scroll;"
-          v-for="(cryptoProfitLog, cryptoIndex) in profitLog"
-          :key="cryptoIndex"
-        >
+      <section id="logFileSection" v-show="step == 'logs'">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="card-title">Add an<span v-show="tradeLog.length > 0">other</span> exchange log file</h2>
+            <div class="form-group row">
+              <label for="logType" class="col-sm-2 col-form-label">Exchange:</label>
+              <div class="col-sm-10">
+                <select class="form-control" id="logType" v-model="logType">
+                  <option value="kraken">Kraken</option>
+                  <option value="bitstamp">Bitstamp</option>
+                </select>
+              </div>
+            </div>
 
-        <h2>{{ cryptoIndex }}: {{getCryptoProfit(cryptoProfitLog)}}</h2>
+            <div class="form-group row">
+              <label for="logFileInput" class="col-sm-2 col-form-label">Select exchange log file (.csv)</label>
+              <div class="col-sm-10">
+                <input type="file" class="form-control-file" id="logFileInput" ref="logFileInput">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <button type="button" class="btn btn-primary" @click.prevent="processLog">Add and process exchange log</button>
+              </div>
+              
+            </div>
 
-        <table class="table table-sm">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Date</th>
-              <th scope="col">Volume</th>
-              <th scope="col">Buy Price</th>
-              <th scope="col">Sell Price</th>
-              <th scope="col">Total</th>
-              <th scope="col">Profit/Loss</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(trade, index) in cryptoProfitLog"
-              :key="index"
-            >
-              <th scope="row">{{ index + 1 }}</th>
-              <td>{{ formatDate(trade.timestamp) }}</td>
-              <td>{{ trade.volume }}</td>
-              <td>{{ trade.buyPrice }}</td>
-              <td>{{ trade.sellPrice }}</td>
-              <td>{{ trade.volume * trade.sellPrice }}</td>
-              <td>{{ trade.profit }}</td>
-            </tr>
-          </tbody>
-        </table>
-
-        </div>
-
-        <h1>Total profit/loss: {{ getTotalProfit() }}</h1>
-        <div class="col-12">
-          <div class="form-group">
-            <label for="profitLogCsv">Profit log CSV</label>
-            <textarea class="form-control" id="profitLogCsv" rows="8" v-model="profitLogCsv"></textarea>
           </div>
         </div>
 
-      </div>
+
+        <div class="card mt-2 border-success" v-show="tradeLog.length > 0">
+          <div class="card-header">
+            Log items processed: <a href="#" @click.prevent="showTradeLog = !showTradeLog">{{ tradeLog.length }}</a>
+          </div>
+          <div class="card-body" v-show="showTradeLog">
+            <table class="table table-sm">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Operation</th>
+                  <th scope="col">Crypto</th>
+                  <th scope="col">Volume</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(trade, index) in tradeLog"
+                  :key="index"
+                >
+                  <th scope="row">{{ index + 1 }}</th>
+                  <td>{{ formatDate(trade.timestamp) }}</td>
+                  <td>{{ trade.operation }}</td>
+                  <td>{{ trade.crypto }}</td>
+                  <td>{{ trade.volume }}</td>
+                  <td>{{ trade.price }}</td>
+                  <td>{{ trade.volume * trade.price }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="card-footer">
+            <button type="button" class="btn btn-success btn-lg" @click.prevent="calculatePnL">Calculate Profit/Loss</button>
+          </div>
+        </div>
+      </section>
+
+      <section id="resultsSection" v-show="step == 'results'">
+        <div class="row">
+          <div class="col-12" style="max-height: 400px; overflow: scroll;"
+            v-for="(cryptoProfitLog, cryptoIndex) in profitLog"
+            :key="cryptoIndex"
+          >
+
+          <h2>{{ cryptoIndex }}: {{getCryptoProfit(cryptoProfitLog)}}</h2>
+
+          <table class="table table-sm">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Date</th>
+                <th scope="col">Volume</th>
+                <th scope="col">Buy Price</th>
+                <th scope="col">Sell Price</th>
+                <th scope="col">Total</th>
+                <th scope="col">Profit/Loss</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(trade, index) in cryptoProfitLog"
+                :key="index"
+              >
+                <th scope="row">{{ index + 1 }}</th>
+                <td>{{ formatDate(trade.timestamp) }}</td>
+                <td>{{ trade.volume }}</td>
+                <td>{{ trade.buyPrice }}</td>
+                <td>{{ trade.sellPrice }}</td>
+                <td>{{ trade.volume * trade.sellPrice }}</td>
+                <td>{{ trade.profit }}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          </div>
+
+          <h1>Total profit/loss: {{ getTotalProfit() }}</h1>
+          <div class="col-12">
+            <div class="form-group">
+              <label for="profitLogCsv">Profit log CSV</label>
+              <textarea class="form-control" id="profitLogCsv" rows="8" v-model="profitLogCsv"></textarea>
+            </div>
+          </div>
+        </div>
+      </section>
       
     </form>
   </div>
@@ -137,17 +179,39 @@ export default Vue.extend({
   // },
   data() {
     return {
+      step: "intro",
+      logType: "",
       tradeLog: new Array<TradeLogItem>(),
+      showTradeLog: false,
       profitLog: <CryptoProfitLog>{},
       profitLogCsv: "",
-      errors: new Array<string>()
+      errors: new Array<string>(),
+      showErrorDetails: false
     }
   },
   methods: {
-    setLogFile: function(file: File) {
-      logFile = file;
-      // console.log(logFile);
-      
+    goToLogStep: function() {
+      this.step = 'logs';
+    },
+
+    processLog: function() {
+      let fileInput = <HTMLInputElement>this.$refs.logFileInput;
+      if (this.logType != "" && fileInput.files) {
+        logFile = fileInput.files[0];
+        switch(this.logType) {
+          case "kraken": {
+            this.parseKrakenLog();
+            break;
+          }
+          case "bitstamp": {
+            this.parseBitstampLog();
+            break;
+          }
+        }
+        (<HTMLInputElement>this.$refs.logFileInput).value = "";
+        this.logType = "";
+
+      }
     },
 
     parseKrakenLog: function() {
@@ -264,6 +328,8 @@ export default Vue.extend({
 
 
     calculatePnL: function() {
+      this.step = "results";
+      
       this.tradeLog.sort((a, b) => { return (a.timestamp < b.timestamp ? -1 : 1) });
 
       // get all currencies
@@ -358,3 +424,9 @@ export default Vue.extend({
   }
 });
 </script>
+<style lang="scss" scoped>
+.home {
+  background-color: #ffffff;
+  border: 1px solid #eeeeee;
+}
+</style>
